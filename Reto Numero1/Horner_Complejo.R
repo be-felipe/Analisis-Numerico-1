92cf94ffd4179363bd8e980b0934de122c8f5eea
Camilo Andres Sandoval Guayambuco <60140405+camilosan10@users.noreply.github.com>
2020-02-22 19:34:45 -0500

    Create Horner_Complejo.R

---
+++ Reto Numero1/Horner_Complejo.R
@@ -0,0 +1,16 @@
+#########################
+##Horner reto punto 2
+#########################
+
+res=0
+coeficientesI=c(1,10,8,0)
+res=0;
+resultado = c(complex())
+x=complex(real=2,imaginary=1)
+n=1
+while(n<5){
+  res=res*x+coeficientesI[n]
+  resultado[n]= res;
+  n=n+1;
+}
+Tablita = data.frame(resultado)
